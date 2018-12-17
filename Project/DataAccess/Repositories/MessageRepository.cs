006480d3c457c9e54879d2e9a1fca96dc23f57a9
Kizlo Taras <orleon.nilkford@gmail.com>
2018-12-17 23:03:18 +0200

    Add files via upload

---
+++ Project/DataAccess/Repositories/MessageRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace DataAccess.Repositories
+{
+    public class MessageRepository : GenericRepository<Entities.Message>
+    {
+        public MessageRepository(Context.AppContext context) : base(context)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
