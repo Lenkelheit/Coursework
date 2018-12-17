018540c0b749a9638458523fd0611dd0ec3f50cb
Kizlo Taras <orleon.nilkford@gmail.com>
2018-12-17 23:06:13 +0200

    Add files via upload

---
+++ Project/UnitTest/DataAccess/Repositories/SubjectRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.DataAccess.Repositories
+{
+    [TestClass]
+    public class SubjectRepository
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
