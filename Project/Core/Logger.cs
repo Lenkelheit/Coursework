fc678d11859fcaeeddc591f324d8be0a6c6fffe2
Kizlo Taras <orleon.nilkford@gmail.com>
2018-12-17 11:52:09 +0200

    Update Logger.cs

--- Project/Core/Logger.cs
+++ Project/Core/Logger.cs
@@ -1,10 +1,13 @@
-﻿namespace Core
+namespace Core
 {
     public static class Logger
     {
-        public static void Log(string message)
+        public static void Log(LogMode logMode, string message)
         {
-            System.IO.File.AppendAllText(Configuration.AppConfig.LOG_FILE, message);
+            System.IO.File.AppendAllText(
+                path: Configuration.AppConfig.LOG_FILE, 
+                contents: string.Format(Configuration.AppConfig.LOG_TEMPLATE_FORMAT, System.DateTime.Now, logMode , message) 
+                );
         }
     }
 }
