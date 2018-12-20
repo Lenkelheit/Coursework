49444e54a550c144600e890f0010e1141b6f174e
Kizlo Taras <orleon.nilkford@gmail.com>
2018-12-20 14:18:56 +0200

    Add files via upload

---
+++ Project/UnitTest/AdditionalAsserts/ExceptionAssert.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTest
+{
+    /// <summary>
+    /// Contains logic to check exceptions.
+    /// </summary>
+    public static class ExceptionAssert
+    {
+        /// <summary>
+        /// Checks if the input delegate throws a exception of type TException.
+        /// </summary>
+        /// <typeparam name="TException">The type of exception expected.</typeparam>
+        /// <param name="methodToExecute">The method to execute to generate the exception.</param>
+        public static void Throws<TException>(Action methodToExecute) where TException : Exception
+        {
+            try
+            {
+                methodToExecute();
+                Assert.Fail("Expected exception of type " + typeof(TException) + " but no exception was thrown.");
+            }
+            catch (TException)
+            {
+                return;
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("Expected exception of type " + typeof(TException) + " but type of " + ex.GetType() + " was thrown instead.");
+            }
+        }
+    }
+}
