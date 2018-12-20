e561a9e63bc2de8c75c20a1e40515cf4860764ea
Kizlo Taras <orleon.nilkford@gmail.com>
2018-12-20 15:14:35 +0200

    Update SettingViewModel.cs

--- Project/UnitTest/ViewModel/ViewModels/User/SettingViewModel.cs
+++ Project/UnitTest/ViewModel/ViewModels/User/SettingViewModel.cs
@@ -9,22 +9,27 @@ namespace UnitTest.ViewModel.ViewModels.User
         [TestMethod]
         public void User()
         {
+            Assert.Fail();
         }
         [TestMethod]
         public void TempAvatarPath()
         {
+            Assert.Fail();
         }
         [TestMethod]
         public void Nickname()
         {
+            Assert.Fail();
         }
         [TestMethod]
         public void Password()
         {
+            Assert.Fail();
         }
         [TestMethod]
         public void NewPassword()
         {
+            Assert.Fail();
         }
     }
 }
