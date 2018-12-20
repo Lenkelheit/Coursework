3f7eb0cdf164ee3cc3ba02c2c8fdf0607ebe3876
Kizlo Taras <orleon.nilkford@gmail.com>
2018-12-20 15:11:17 +0200

    Update GenericRepositories.cs

--- Project/UnitTest/DataAccess/Repositories/GenericRepositories.cs
+++ Project/UnitTest/DataAccess/Repositories/GenericRepositories.cs
@@ -9,34 +9,42 @@ namespace UnitTest.DataAccess.Repositories
         [TestMethod]
         public void Constructors()
         {
+            Assert.Fail();
         }
         [TestMethod]
         public void Count()
         {
+            Assert.Fail();
         }
         [TestMethod]
         public void Get()
         {
+            Assert.Fail();
         }
         [TestMethod]
         public void GetById()
         {
+            Assert.Fail();
         }
         [TestMethod]
         public void Insert()
         {
+            Assert.Fail();
         }
         [TestMethod]
         public void DeleteById()
         {
+            Assert.Fail();
         }
         [TestMethod]
         public void DeleteByValue()
         {
+            Assert.Fail();
         }
         [TestMethod]
         public void Update()
         {
+            Assert.Fail();
         }
     }
 }
