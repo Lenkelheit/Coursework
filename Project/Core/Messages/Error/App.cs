2fec94ae83778e6896022f5d21fbf2cad6f13f74
iamprovidence <orleon.nilkford@gmail.com>
2019-01-22 13:10:24 +0200

    Log Fatal error. resolve#86

---
+++ Project/Core/Messages/Error/App.cs
@@ -0,0 +1,13 @@
+﻿namespace Core.Messages.Error
+{
+    /// <summary>
+    /// Consists of all important messages that can be thrown in project.
+    /// </summary>
+    public static class App
+    {
+        /// <summary>
+        /// Used when program has been closed with fatal unhandled error
+        /// </summary>
+        public static readonly string FATAL_ERROR_CLOSE = string.Concat("The program has been shut down with error", System.Environment.NewLine, "Please accept our apologies");
+    }
+}
