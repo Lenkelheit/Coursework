0a5188dd565f2aea125dde754412227fc19973c3
Nazar <tymtsiv2710@gmail.com>
2019-02-03 16:48:23 +0200

    Set up that one instance of the application can be run at once. Close#87.

--- Project/Galagram/App.xaml.cs
+++ Project/Galagram/App.xaml.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using System.Linq;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Windows.Threading;
+
+using Core.Configuration;
 
 namespace Galagram
 {
@@ -8,6 +13,14 @@ namespace Galagram
     /// </summary>
     public partial class App : System.Windows.Application
     {
+        // FIELDS
+        private static Mutex mutex;
+
+        // EXTERN METHOD
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(System.IntPtr hWnd);
+
+        //METHODS
         /// <summary>
         /// Raises <see cref="System.Windows.Application.Startup"/> event.
         /// </summary>
@@ -16,7 +29,26 @@ namespace Galagram
         /// </param>
         protected override void OnStartup(System.Windows.StartupEventArgs e)
         {
+            // It is for single instance of application, when other application will be created it will redirect to first main one
+            // and that other will be closed.
+            bool isCreatedNewMutex;
+            mutex = new Mutex(initiallyOwned: true, name: AppConfig.APP_NAME + "Mutex", createdNew: out isCreatedNewMutex);
+
+            if (!isCreatedNewMutex)
+            {
+                int currentProcessId = Process.GetCurrentProcess().Id;
+                Process process = Process.GetProcessesByName(AppConfig.APP_NAME).First(p => p.Id != currentProcessId);
+
+                Core.Logger.GetLogger.Log(Core.LogMode.Info, System.Environment.NewLine + "Second application has been started up - redirected to main one");
+
+                SetForegroundWindow(process.MainWindowHandle);
+
+                this.Shutdown();
+                return;
+            }
+
             Core.Logger.GetLogger.LogAsync(Core.LogMode.Info, System.Environment.NewLine + "Application has been started up");
+
             this.DispatcherUnhandledException += FatalClose;
             base.OnStartup(e);
         }
@@ -25,7 +57,7 @@ namespace Galagram
         {
             // it will not work on Debug mode
             // log all exception and inners one
-            for (Exception exception = e.Exception; exception != null; exception = exception.InnerException)
+            for (System.Exception exception = e.Exception; exception != null; exception = exception.InnerException)
             {
                 Core.Logger.GetLogger.LogAsync(Core.LogMode.Fatal, $"The program has been closed with fatal unhandled error. Exception: {exception}");
             }
@@ -33,7 +65,7 @@ namespace Galagram
 
             Services.WindowManager.Instance.ShowMessageWindow(Core.Messages.Error.App.FATAL_ERROR_CONTINUE);
 
-            if (Core.Configuration.AppConfig.DO_CLOSE_APP_ON_FATAL_ERROR)
+            if (AppConfig.DO_CLOSE_APP_ON_FATAL_ERROR)
             {
                 this.Shutdown();
             }
